8cf3f5a3f69b114e7144b59246c9269df183a653
robertdenilo <b47849@hotmail.com>
2018-09-12 15:52:44 -0400

    180912-2

--- src/main/java/com/rabbitmq/demo/Receiver.java
+++ src/main/java/com/rabbitmq/demo/Receiver.java
@@ -8,6 +8,7 @@ public class Receiver {
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
         latch.countDown();
+        
     }
 
     public CountDownLatch getLatch() {
