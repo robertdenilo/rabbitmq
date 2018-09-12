3542f11ccd703fc8683990d01b59448871ecfe5c
robertdenilo <b47849@hotmail.com>
2018-09-12 15:47:36 -0400

    180912

---
+++ src/main/java/com/rabbitmq/demo/RabbitConfig.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public Queue Queue() {
+        return new Queue("hello-mq");
+    }
+}
